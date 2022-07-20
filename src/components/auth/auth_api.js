056961cdad35697bf5e334a09ab381f3779e4f56
Oluwaniyii <ayodeleyniyii@gmail.com>
2022-07-20 18:56:44 +0100

    Add API Testing sample  (NOTE: this is a rough attempt) disable snyk test as it brings uknown errors

--- src/components/auth/auth_api.js
+++ src/components/auth/auth_api.js
@@ -1,5 +1,18 @@
 const router = require("express").Router();
+const Login = require("./Login");
+const LoginRepository = require("./Repository/User/UserRepository");
 
-/** */
+router.post("/login", async (req, res, next) => {
+  try {
+    const email = req.body["email"];
+    const password = req.body["password"];
+    const repository = new LoginRepository();
+    const login = new Login(repository);
+    const action = await login.init(email, password);
+    res.json(action);
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
--- src/controller.js
+++ src/controller.js
@@ -1,3 +1,11 @@
+const removeSubdocs = require("mongoose/lib/plugins/removeSubdocs");
+
 const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  res.json({ hello: "hi" });
+});
+
+router.use("/auth", require("./components/auth/auth_api"));
 
 module.exports = router;
